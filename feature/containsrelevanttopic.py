069c91942339110f8241e374f57c14ac8e82ef0d
simonedelmonte92 <simone.delmonte92@gmail.com>
2015-06-20 12:08:25 +0200

    added the following features: ContainsRelevantTopic

--- feature/containsrelevanttopic.py
+++ feature/containsrelevanttopic.py
@@ -7,9 +7,9 @@ class ContainsRelevantTopic(FeatureExtractorI):
     def __init__(self, train_sample, at_most=200):
         cnt = Counter()
         for (_, given_features, _) in train_sample:
-            for key in given_features['topicIds']:
+            for key in given_features['relevantTopicIds']:
                 cnt[key] += 1
         self.attributes = {key: 'numeric' for (key, _) in cnt.most_common(at_most)}
 
     def extract(self, data):
-        return {key: '1' for key in data['topicIds']}
+        return {key: '1' for key in data['relevantTopicIds']}
