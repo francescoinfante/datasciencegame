100404842faabc8799f2def62c7295baf8660f3d
Francesco Infante <fra.inf@icloud.com>
2015-06-20 16:55:22 +0200

    at_most

--- feature/containstopicunited.py
+++ feature/containstopicunited.py
@@ -4,13 +4,12 @@ from api import FeatureExtractorI
 
 
 class ContainsTopicUnited(FeatureExtractorI):
-    def __init__(self, train_sample, min_freq=100):
+    def __init__(self, train_sample, at_most=500):
         cnt = Counter()
         for (_, given_features, _) in train_sample:
             for key in given_features['topicIds'] | given_features['relevantTopicIds']:
                 cnt[key] += 1
-        self.attributes = {key: 'numeric' for (key, value) in cnt.iteritems() if value >= min_freq}
-        print "Number of topics: " + str(len(self.attributes))
+        self.attributes = {key: 'numeric' for (key, _) in cnt.most_common(at_most)}
 
     def extract(self, data):
         return {key: '1' for key in data['topicIds'] | data['relevantTopicIds']}
