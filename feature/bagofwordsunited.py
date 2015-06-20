af49881d5323befb515cfddc9ffcb81978c05a7a
Francesco Infante <fra.inf@icloud.com>
2015-06-20 20:15:16 +0200

    corrected bagofwords

--- feature/bagofwordsunited.py
+++ feature/bagofwordsunited.py
@@ -7,20 +7,18 @@ from utility import tokenize
 class BagOfWordsUnited(FeatureExtractorI):
     def __init__(self, train_sample, at_most=600):
         cnt = Counter()
-        for _, given_features, _ in train_sample:
-            cnt[given_features['title']] += 1
-            cnt[given_features['description']] += 1
+        for _, data, _ in train_sample:
+            tokens = tokenize(data['title'] + ' ' + data['description'])
+            for x in tokens:
+                cnt[x] += 1
 
         self.attributes = dict([(x, 'numeric') for x in cnt.most_common(at_most)])
 
     def extract(self, data):
-        tokens = tokenize(data['title'])
-        tokens_desc = tokenize(data['description'])
+        tokens = tokenize(data['title'] + ' ' + data['description'])
 
         res = {}
         for x in tokens:
-            res[x] = 1
-        for x in tokens_desc:
             res[x] = 1
 
         return res
