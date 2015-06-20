8de1f241c9cfd2916b6b1463970e6b4952c4cba3
Francesco Infante <fra.inf@icloud.com>
2015-06-20 12:51:10 +0200

    small fix

--- utility/dataparser.py
+++ utility/dataparser.py
@@ -14,6 +14,14 @@ def array_to_dict(row, array):
     if len(array) != 15:
         raise Warning('Number of fields of row ' + str(row) + ' is ' + str(len(array)))
 
+    test = array[9].lower()
+    if test not in ['3d', '2d']:
+        logging.warn('dimension not valid for ' + str(row))
+
+    test = array[10].lower()
+    if test not in ['hd', 'sd']:
+        logging.warn('definition not valid for ' + str(row))
+
     test = array[11].lower()
     if test not in ['false', 'true']:
         logging.warn('caption not valid for ' + str(row))
