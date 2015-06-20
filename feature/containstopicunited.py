3cab6adb3bd016697fed34c3dda61b96b43fb3bc
Francesco Infante <fra.inf@icloud.com>
2015-06-20 16:38:59 +0200

    small fix

--- feature/containstopicunited.py
+++ feature/containstopicunited.py
@@ -4,12 +4,10 @@ from api import FeatureExtractorI
 
 
 class ContainsTopicUnited(FeatureExtractorI):
-    def __init__(self, train_sample, min_freq=5):
+    def __init__(self, train_sample, min_freq=100):
         cnt = Counter()
         for (_, given_features, _) in train_sample:
-            for key in given_features['topicIds']:
-                cnt[key] += 1
-            for key in given_features['relevantTopicIds']:
+            for key in given_features['topicIds'] | given_features['relevantTopicIds']:
                 cnt[key] += 1
         self.attributes = {key: 'numeric' for (key, value) in cnt.iteritems() if value >= min_freq}
         print "Number of topics: " + str(len(self.attributes))
