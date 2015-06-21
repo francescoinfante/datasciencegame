b6037fd2f3ac8414fd8029945e355e7ae1372bc3
Francesco Infante <fra.inf@icloud.com>
2015-06-21 14:41:31 +0200

    configurable news

--- feature/newbag.py
+++ feature/newbag.py
@@ -1,28 +1,24 @@
-from collections import Counter
-
 from feature.api import FeatureExtractorI
 from utility import tokenize
 
 
 class NewBag(FeatureExtractorI):
-    def __init__(self, train_sample, ngram_size, progressbar):
-        self.cnt = Counter()
+    def __init__(self, train_sample, ngram_size, list_of_attributes, progressbar):
+        self.feat_set = set()
         self.ngram_size = ngram_size
+        self.list_of_attributes = list_of_attributes
         for _, data, _ in train_sample:
-            tmp_set = set()
-            for token in tokenize(data['title'] + ' ' + data['description'], self.ngram_size):
-                tmp_set.add(token)
-            for token in tmp_set:
-                self.cnt[token] += 1
+            for attr in self.list_of_attributes:
+                for token in tokenize(data[attr], self.ngram_size):
+                    self.feat_set.add(token)
             progressbar.update(progressbar.currval + 1)
 
-        self.attributes = dict([(x, 'numeric') for x in self.cnt])
+        self.attributes = dict([(x, 'numeric') for x in self.feat_set])
 
-    def extract(self, data, norm=True):
-        tokens = tokenize(data['title'] + ' ' + data['description'], self.ngram_size)
-        count = Counter()
+    def extract(self, data):
+        tokens = []
 
-        for x in tokens:
-            count[x] += 1
+        for attr in self.attributes:
+            tokens.extend(tokenize(data[attr], self.ngram_size))
 
-        return dict(count)
+        return {key: 1 for key in tokens}
--- feature/newtopic.py
+++ feature/newtopic.py
@@ -1,19 +1,22 @@
-from collections import Counter
-
 from api import FeatureExtractorI
 
 
 class NewTopic(FeatureExtractorI):
-    def __init__(self, train_sample, progressbar):
-        self.cnt = Counter()
-        for _, given_features, _ in train_sample:
-            for key in given_features['topicIds'] | given_features['relevantTopicIds']:
-                self.cnt[key] += 1
+    def __init__(self, train_sample, list_of_attributes, progressbar):
+        self.feat_set = set()
+        self.list_of_attributes = list_of_attributes
+        for _, data, _ in train_sample:
+            for attr in self.list_of_attributes:
+                for key in data[attr]:
+                    self.feat_set.add(key)
             progressbar.update(progressbar.currval + 1)
 
-        self.attributes = dict([(x, 'numeric') for x in self.cnt])
+        self.attributes = dict([(x, 'numeric') for x in self.feat_set])
 
     def extract(self, data):
-        topics = data['topicIds'] | data['relevantTopicIds']
+        topics = []
+
+        for attr in self.list_of_attributes:
+            topics.extend(data[attr])
 
         return {key: 1 for key in topics}
