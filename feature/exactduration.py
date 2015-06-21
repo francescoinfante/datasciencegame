df53fb1a04bfe842a882813f15052bbd5851682c
RncLsn <rnc.lsn@gmail.com>
2015-06-21 11:13:33 +0200

    Random forest, plus optional normalisation.

--- feature/exactduration.py
+++ feature/exactduration.py
@@ -2,12 +2,17 @@ from api import FeatureExtractorI
 
 
 class ExactDuration(FeatureExtractorI):
-    def __init__(self, train_sample, test_sample):
-        maximum = 1
-        for instance in zip(*train_sample)[1] + zip(*test_sample)[1]:
-            maximum = max(maximum, instance['exactduration'])
-        self.maximum = maximum
+    def __init__(self, train_sample, test_sample, normalize=False):
+        self.normalize = normalize
+        if normalize:
+            maximum = 1
+            for instance in zip(*train_sample)[1] + zip(*test_sample)[1]:
+                maximum = max(maximum, instance['exactduration'])
+            self.maximum = maximum
         self.attributes = {'exactduration': 'numeric'}
 
     def extract(self, data):
-        return {'exactduration': float(data['duration']) / self.maximum}
+        if self.normalize:
+            return {'exactduration': float(data['duration']) / self.maximum}
+        else:
+            return {'exactduration': data['duration']}
