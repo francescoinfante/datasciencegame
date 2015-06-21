bd0a2d278b09ac0a2242bdd55c4ba02189183f26
RncLsn <rnc.lsn@gmail.com>
2015-06-21 09:09:03 +0100

    Normalize CounterFit.

--- feature/counterfit.py
+++ feature/counterfit.py
@@ -2,7 +2,16 @@ from api import FeatureExtractorI
 
 
 class CounterFit(FeatureExtractorI):
-    def __init__(self):
+    def __init__(self, train_sample, test_sample):
+        maximums = {'viewCount': 1, 'likeCount': 1, 'dislikeCount': 1, 'favoriteCount': 1, 'commentCount': 1}
+        for instance in train_sample + test_sample:
+            maximums['viewCount'] = max(maximums['viewCount'], instance['viewCount'])
+            maximums['likeCount'] = max(maximums['likeCount'], instance['likeCount'])
+            maximums['dislikeCount'] = max(maximums['dislikeCount'], instance['dislikeCount'])
+            maximums['favoriteCount'] = max(maximums['favoriteCount'], instance['favoriteCount'])
+            maximums['commentCount'] = max(maximums['commentCount'], instance['commentCount'])
+        self.maximums = maximums
+
         self.attributes = {
             'viewCount': 'numeric',
             'likeCount': 'numeric',
@@ -11,5 +20,12 @@ class CounterFit(FeatureExtractorI):
             'commentCount': 'numeric'}
 
     def extract(self, data):
-        return {'viewCount': data['viewCount'], 'likeCount': data['likeCount'], 'dislikeCount': data['dislikeCount'],
-                'favoriteCount': data['favoriteCount'], 'commentCount': data['commentCount']}
+        view_count = float(data['viewCount']) / self.maximums['viewCount'] if data['viewCount'] != 0 else 0
+        like_count = float(data['likeCount']) / self.maximums['likeCount'] if data['likeCount'] != 0 else 0
+        dislike_count = float(data['dislikeCount']) / self.maximums['dislikeCount'] if data['dislikeCount'] != 0 else 0
+        favorite_count = float(data['favoriteCount']) / self.maximums['favoriteCount'] \
+            if data['favoriteCount'] != 0 else 0
+        comment_count = float(data['commentCount']) / self.maximums['commentCount'] if data['commentCount'] != 0 else 0
+
+        return {'viewCount': view_count, 'likeCount': like_count, 'dislikeCount': dislike_count,
+                'favoriteCount': favorite_count, 'commentCount': comment_count}
