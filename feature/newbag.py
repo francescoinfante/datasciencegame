502f3760bc2c3407acb290f2620286439f032c81
Francesco Infante <fra.inf@icloud.com>
2015-06-21 15:38:52 +0200

    added min freq

--- feature/newbag.py
+++ feature/newbag.py
@@ -1,17 +1,20 @@
+from collections import Counter
+
 from feature.api import FeatureExtractorI
 from utility import tokenize
 
 
 class NewBag(FeatureExtractorI):
-    def __init__(self, train_sample, ngram_size, list_of_attributes, progressbar):
-        self.attributes = {}
+    def __init__(self, train_sample, ngram_size, list_of_attributes, min_frequency, progressbar):
+        self.count = Counter()
         self.ngram_size = ngram_size
         self.list_of_attributes = list_of_attributes
         for _, data, _ in train_sample:
             for attr in self.list_of_attributes:
                 for token in tokenize(data[attr], self.ngram_size):
-                    self.attributes[token] = 'numeric'
+                    self.count[token] += 1
             progressbar.update(progressbar.currval + 1)
+        self.attributes = {key: 'numeric' for key, value in self.count if value >= min_frequency}
 
     def extract(self, data):
         tokens = []
--- feature/newtopic.py
+++ feature/newtopic.py
@@ -1,15 +1,18 @@
+from collections import Counter
+
 from api import FeatureExtractorI
 
 
 class NewTopic(FeatureExtractorI):
-    def __init__(self, train_sample, list_of_attributes, progressbar):
-        self.attributes = {}
+    def __init__(self, train_sample, list_of_attributes, min_frequency, progressbar):
+        self.count = Counter()
         self.list_of_attributes = list_of_attributes
         for _, data, _ in train_sample:
             for attr in self.list_of_attributes:
                 for topic in data[attr]:
-                    self.attributes[topic] = 'numeric'
+                    self.count[topic] += 1
             progressbar.update(progressbar.currval + 1)
+        self.attributes = {key: 'numeric' for key, value in self.count if value >= min_frequency}
 
     def extract(self, data):
         topics = []
