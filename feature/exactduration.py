07e2ae5f6a976698826eaeab2320fef9c7100b3a
RncLsn <rnc.lsn@gmail.com>
2015-06-21 09:13:19 +0100

    Normalize ExactDuration.

--- feature/exactduration.py
+++ feature/exactduration.py
@@ -2,8 +2,12 @@ from api import FeatureExtractorI
 
 
 class ExactDuration(FeatureExtractorI):
-    def __init__(self):
+    def __init__(self, train_sample, test_sample):
+        maximum = 1
+        for instance in train_sample + test_sample:
+            maximum = max(maximum, instance['exactduration'])
+        self.maximum = maximum
         self.attributes = {'exactduration': 'numeric'}
 
     def extract(self, data):
-        return {'exactduration': data['duration']}
+        return {'exactduration': float(data['duration']) / self.maximum if data['duration'] != 0 else 0}
